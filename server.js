ecf4390f399c0afa715083580c7132afc5d5206f
SornchaiTheDev <sornchaithedev@gmail.com>
2024-07-07 16:18:49 +0700

    feat: add recommend replies api endpoint

--- server.js
+++ server.js
@@ -1,57 +1,58 @@
-const express = require('express');
-const { storage } = require('./storage/storage');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const socketIOMiddleware = require('./middlewares/socketIOMiddleware');
-const socketIOChatMiddleware = require('./middlewares/socketIOChatMiddleware');
-const auth = require('./middlewares/authMiddleware');
+const express = require("express");
+const { storage } = require("./storage/storage");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const socketIOMiddleware = require("./middlewares/socketIOMiddleware");
+const socketIOChatMiddleware = require("./middlewares/socketIOChatMiddleware");
+const auth = require("./middlewares/authMiddleware");
 
 const app = express();
 const port = 8080;
-const cors = require('cors');
+const cors = require("cors");
 dotenv.config();
 app.use(cors());
 
-const multer = require('multer');
+const multer = require("multer");
 const upload = multer({ storage });
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true
-}).then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error(err));
-
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error(err));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const server = socketIOChatMiddleware(app);
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-}
-);
-
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 //routes
-const characterRoute = require('./routes/characterRoute');
-const knowledgeRoute = require('./routes/knowledgeRoute');
-const folderRoute = require('./routes/folderRoute');
-const userRoute = require('./routes/userRoute');
-const newsRoute = require('./routes/newsRoute');
-const aiRoute = require('./routes/aiRoute');    
+const characterRoute = require("./routes/characterRoute");
+const knowledgeRoute = require("./routes/knowledgeRoute");
+const folderRoute = require("./routes/folderRoute");
+const userRoute = require("./routes/userRoute");
+const newsRoute = require("./routes/newsRoute");
+const aiRoute = require("./routes/aiRoute");
+const recommendRoute = require("./routes/recommendRoute");
 
 //routes add auth middleware
-app.use('/character', characterRoute);
-app.use('/knowledge', auth, knowledgeRoute);
-app.use('/folder', auth,folderRoute);
-app.use('/user', userRoute);
-app.use('/news', newsRoute);
-app.use('/ai', aiRoute);
+app.use("/character", characterRoute);
+app.use("/knowledge", auth, knowledgeRoute);
+app.use("/folder", auth, folderRoute);
+app.use("/user", userRoute);
+app.use("/news", newsRoute);
+app.use("/ai", aiRoute);
+app.use("/ai", recommendRoute);
 
-app.post('/upload', upload.single('image'), (req, res) => {
-    res.send('Done');
+app.post("/upload", upload.single("image"), (req, res) => {
+  res.send("Done");
 });
 
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-}
-);
+  console.log(`Server is running on port ${port}`);
+});
+
